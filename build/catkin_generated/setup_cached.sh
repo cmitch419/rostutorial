a2600b6833f0394a804db669b957e651ed96a109
cmitch419 <cmitch419@gmail.com>
2021-04-01 13:38:59 -0400

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cmitch/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/cmitch/.rvm/gems/ruby-3.0.0/bin:/home/cmitch/.rvm/gems/ruby-3.0.0@global/bin:/home/cmitch/.rvm/rubies/ruby-3.0.0/bin:/home/cmitch/gems/bin:/home/cmitch/.nvm/versions/node/v12.14.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/mnt/c/Program Files/WindowsApps/CanonicalGroupLimited.UbuntuonWindows_2004.2021.222.0_x64__79rhkp1fndgsc:/mnt/c/Program Files/Common Files/Oracle/Java/javapath:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/Program Files/Oculus/Support/oculus-runtime:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/Program Files/NVIDIA Corporation/NVIDIA NvDLISR:/mnt/c/Program Files/Git/cmd:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/Microsoft SQL Server/130/Tools/Binn/:/mnt/c/Program Files/Microsoft SQL Server/Client SDK/ODBC/170/Tools/Binn/:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/Program Files/dotnet/:/mnt/c/Users/cmitc/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/cmitc/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/cmitc/.dotnet/tools:/snap/bin:/home/cmitch/.rvm/bin:/mnt/d/platform-tools:/home/cmitch/.rvm/bin'
+export PWD='/home/cmitch/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cmitch/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cmitch/catkin_ws/src:$ROS_PACKAGE_PATH"
